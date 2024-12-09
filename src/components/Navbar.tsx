e42d130bec06c399b1c16fc387074bcb4e51ba10
pouria-mobaraki <ss_pooria69@yahoo.com>
2024-12-09 13:04:07 +0330

    create project

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { AiOutlineUser } from 'react-icons/ai' 
+import { FiShoppingCart } from 'react-icons/fi'
+
+function Navbar() {
+  return (
+    <div className='pt-4 bg-white top-0 sticky'>
+        s
+    </div>
+  )
+}
+
+export default Navbar
